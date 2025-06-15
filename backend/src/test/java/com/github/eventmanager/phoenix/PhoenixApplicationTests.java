2c0fcac11b9e45cc67fe65478a6c9f637033ce43
TheBloodyAmateur <botan_kadir@outlook.com>
2025-06-15 15:13:58 +0200

    Add project structure and Spring Boot initializer to the project

---
+++ backend/src/test/java/com/github/eventmanager/phoenix/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eventmanager.phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
