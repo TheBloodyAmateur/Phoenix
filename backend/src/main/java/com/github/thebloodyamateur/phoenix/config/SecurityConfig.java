710d46191ef24ba9a4140f80b55623878b923b03
TheBloodyAmateur <botan_kadir@outlook.com>
2025-06-24 11:43:13 +0200

    Adding additional rest controller endpoints to test around with Spring Boot

--- backend/src/main/java/com/github/thebloodyamateur/phoenix/config/SecurityConfig.java
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.github.thebloodyamateur.phoenix;
+package com.github.thebloodyamateur.phoenix.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/UserController.java
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/UserController.java
@@ -1,23 +1,37 @@
 package com.github.thebloodyamateur.phoenix.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @PostMapping("/user")
+    @PostMapping("/createUser")
     public String createUser() {
         // Logic to create a user would go here
         return "User created successfully!";
     }
 
-    @GetMapping("/user")
+    @GetMapping("/getUser")
     public String getUser() {
         // Logic to retrieve a user would go here
         return "User details retrieved successfully!";
     }
+
+    @GetMapping("/calc")
+    public int getBoolean() {
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4);
+        int result = integerList.stream()
+                .filter(n -> n > 4)
+                .mapToInt(i -> i * 2)
+                .sum();
+        return result;
+    }
+
 }
