85e062ccec4ccfc56d84b912c0ab2b799901cc8e
TheBloodyAmateur <botan_kadir@outlook.com>
2025-06-26 12:28:58 +0200

    Add more endpoints to the User and file controller

--- backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/FileController.java
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/FileController.java
@@ -1,8 +1,43 @@
 package com.github.thebloodyamateur.phoenix.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 
 @RestController
+@RequestMapping("/files")
 public class FileController {
-    
+
+    @GetMapping("file/{id}")
+    public String getFile(@PathVariable String id, @RequestParam String param) {
+        return new String();
+    }
+
+    @PutMapping("file/{id}")
+    public String updateFile(@PathVariable String id, @RequestBody String entity) {
+        //TODO: process PUT request
+        
+        return entity;
+    }
+
+    @DeleteMapping("file/{id}")
+    public String deleteFile(@PathVariable String id) {
+        return "File with ID " + id + " deleted successfully!";
+    }
+
+    @PostMapping("file")
+    public String createFile(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
 }
--- backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/UserController.java
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/controller/UserController.java
@@ -3,35 +3,52 @@ package com.github.thebloodyamateur.phoenix.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
-
-    @PostMapping("/createUser")
-    public String createUser() {
-        // Logic to create a user would go here
-        return "User created successfully!";
+    
+    @GetMapping("/users")
+    public List<String> getAllUsers() {
+        // Logic to retrieve all users would go here
+        return Arrays.asList("User1", "User2", "User3");
     }
 
-    @GetMapping("/getUser")
+    @GetMapping("/user")
     public String getUser() {
         // Logic to retrieve a user would go here
         return "User details retrieved successfully!";
     }
 
-    @GetMapping("/calc")
-    public int getBoolean() {
-        List<Integer> integerList = Arrays.asList(1, 2, 3, 4);
-        int result = integerList.stream()
-                .filter(n -> n > 4)
-                .mapToInt(i -> i * 2)
-                .sum();
-        return result;
+    @PostMapping("/user")
+    public String createUser() {
+        // Logic to create a user would go here
+        return "User created successfully!";
     }
 
+    @PutMapping("/user/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody String entity) {
+        return entity;
+    }
+
+    @PatchMapping("/user/{id}")
+    public String partiallyUpdateUser(@PathVariable String id, @RequestBody String entity) {
+        return entity;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable String id) {
+        // Logic to delete a user by ID would go here
+        return "User with ID " + id + " deleted successfully!";
+    }
 }
