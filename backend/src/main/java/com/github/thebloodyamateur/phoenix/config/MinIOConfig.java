f715dcdfb3303a0b0715e9a4921b568e7f556c22
TheBloodyAmateur <botan_kadir@outlook.com>
2025-09-30 21:22:14 +0200

    New Dockerfile for Backend + new MinioClient bean
    Added a new settings.xml file to accelareate and parallelise the download of maven dependencies. Also added new Service for the Minio client with which file management will be possible.

---
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/config/MinIOConfig.java
@@ -0,0 +1,27 @@
+package com.github.thebloodyamateur.phoenix.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+
+@Configuration
+public class MinIOConfig {
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
--- backend/src/main/java/com/github/thebloodyamateur/phoenix/service/MinIOService.java
+++ backend/src/main/java/com/github/thebloodyamateur/phoenix/service/MinIOService.java
@@ -1,8 +1,12 @@
 package com.github.thebloodyamateur.phoenix.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import io.minio.MinioClient;
 
 @Service
 public class MinIOService {
-
+    @Autowired
+    private MinioClient minioClient;
 }
